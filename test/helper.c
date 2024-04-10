31124c9cabdd1abcd5262f3796209a31e530c1f9
zhoujiandong <fire1220@live.cn>
2024-04-10 22:14:07 +0800

    C的extern关键词

---
+++ test/helper.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// 定义外部变量
+int globalVariable;
+
+// 定义外部函数
+void displayGlobalVariable() {
+    printf("Global Variable: %d\n", globalVariable);
+}
--- test/main.c
+++ test/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+// 声明外部变量
+extern int globalVariable;
+
+// 声明外部函数
+extern void displayGlobalVariable();
+
+// gcc main.c helper.c -o program
 int main() {
-    printf("Test Hello, World!\n");
+    // 使用外部变量
+    globalVariable = 10;
+
+    // 调用外部函数
+    displayGlobalVariable();
+
     return 0;
 }
