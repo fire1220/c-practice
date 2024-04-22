bcdede486c54a5ce510cb2e795d347272ed533ca
zhoujiandong <fire1220@live.cn>
2024-04-22 11:53:59 +0800

    adlist

---
+++ redis/adlist_test.c
@@ -0,0 +1,23 @@
+//
+// Created by fire on 2024/4/22.
+//
+
+// 测试文件
+
+#ifndef TEST_AD_LIST
+    #define TEST_AD_LIST 1
+#endif
+
+#if TEST_AD_LIST==0
+void TestAdList() {
+    printf("测试已经关闭了");
+}
+#else
+void TestAdList() {
+    list *l = listCreate();
+    l = listAddNodeHead(l, sdsnew("篮球1"));
+    l = listAddNodeHead(l, sdsnew("足球2"));
+
+    listPrint(l);
+}
+#endif
