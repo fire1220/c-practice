a8cd461ffb18064fb8e2493f3fd523a3ff8ef0a8
zhoujiandong <fire1220@live.cn>
2024-04-22 14:52:06 +0800

    list

--- redis/main.c
+++ redis/main.c
@@ -3,15 +3,5 @@
 
 int main() {
     TestAdList();
-    // list *x = listCreate();
-    // x = listAddNodeHead(x, "sdfs");
-    // listPrint(x);
-    // sds s = sdsnew("hello world");
-    // printf("%s\n", s);
-    // printf("%zu\n", sdslen(s));
-    // printf("%zu\n", sdsavail(s));
-    // sdsfree(s);
-    // printf("free：%zu\n", sdslen(s));
-
     return 0;
 }
