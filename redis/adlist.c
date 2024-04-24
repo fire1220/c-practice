3d143ccbfcad2c6a1914adf38465047a3ac22df8
zhoujiandong <fire1220@live.cn>
2024-04-24 15:10:49 +0800

    list

--- redis/adlist.c
+++ redis/adlist.c
@@ -68,6 +68,31 @@ list *listAddNodeTail(list *list, void *value)
 
 list *listInsertNode(list *list, listNode *old_node, void *value, int after)
 {
-
+    listNode *node;
+    if ((node = malloc(sizeof(*node))) == NULL) {
+        return NULL;
+    }
+    node->value = value;
+    // 前面插入
+    if (after) {
+        node->prev = old_node;
+        node->next = old_node->next;
+        if (list->tail == old_node) {
+            list->tail = node;
+        }
+    } else {
+        node->prev = old_node->prev;
+        node->next = old_node;
+        if (list->head == old_node) {
+            list->head = node;
+        }
+    }
+    if (node->prev != NULL) {
+        node->prev->next = node;
+    }
+    if (node->next != NULL) {
+        node->next->prev  = node;
+    }
+    list->len++;
     return list;
 }
