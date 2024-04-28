ed18b68de4632e4f1fc0ce27b7fd75c1348ce6b3
zhoujiandong <fire1220@live.cn>
2024-04-28 16:51:33 +0800

    mmap

--- test/main.c
+++ test/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
     printf("hello world");
+    size_t len = 1024;
+    void *ptr = mmap(NULL, len, PROT_READ | PROT_WRITE, MAP_PRIVATE, -1, 0);
+    if (ptr == MAP_FAILED) {
+        perror("mmap");
+        exit(EXIT_FAILURE);
+    }
+    strcpy(ptr, "hello world!");
+    printf("%s\n", (char *)ptr);
+    if (munmap(ptr, len) == -1) {
+        perror("munmap");
+        exit(EXIT_FAILURE);
+    }
     return 0;
 }
