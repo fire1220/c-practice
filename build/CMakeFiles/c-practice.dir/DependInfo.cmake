a250376147422f9a5b59ffabfbf409a6e4e13d5a
fire <fire@firedeMacBook-Air.local>
2025-06-01 01:23:36 +0800

    redis

---
+++ build/CMakeFiles/c-practice.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/fire/c-practice/redis/adlist.c" "CMakeFiles/c-practice.dir/redis/adlist.c.o" "gcc" "CMakeFiles/c-practice.dir/redis/adlist.c.o.d"
+  "/Users/fire/c-practice/redis/adlist_test.c" "CMakeFiles/c-practice.dir/redis/adlist_test.c.o" "gcc" "CMakeFiles/c-practice.dir/redis/adlist_test.c.o.d"
+  "/Users/fire/c-practice/redis/main.c" "CMakeFiles/c-practice.dir/redis/main.c.o" "gcc" "CMakeFiles/c-practice.dir/redis/main.c.o.d"
+  "/Users/fire/c-practice/redis/sds.c" "CMakeFiles/c-practice.dir/redis/sds.c.o" "gcc" "CMakeFiles/c-practice.dir/redis/sds.c.o.d"
+  "/Users/fire/c-practice/redis/sds_test.c" "CMakeFiles/c-practice.dir/redis/sds_test.c.o" "gcc" "CMakeFiles/c-practice.dir/redis/sds_test.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/c-practice.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c-practice.dir/redis/adlist.c.o"
+  "CMakeFiles/c-practice.dir/redis/adlist.c.o.d"
+  "CMakeFiles/c-practice.dir/redis/adlist_test.c.o"
+  "CMakeFiles/c-practice.dir/redis/adlist_test.c.o.d"
+  "CMakeFiles/c-practice.dir/redis/main.c.o"
+  "CMakeFiles/c-practice.dir/redis/main.c.o.d"
+  "CMakeFiles/c-practice.dir/redis/sds.c.o"
+  "CMakeFiles/c-practice.dir/redis/sds.c.o.d"
+  "CMakeFiles/c-practice.dir/redis/sds_test.c.o"
+  "CMakeFiles/c-practice.dir/redis/sds_test.c.o.d"
+  "c-practice"
+  "c-practice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c-practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
